40f5373f09a6fd66359be6d34ef9b9e15603eec2
JCPython <40036093+JCPythonCS@users.noreply.github.com>
2026-09-11 07:29:11 -0400

    app.py

--- app.py
+++ app.py
@@ -266,7 +266,6 @@ with tab_workspace:
         st.markdown("---")
         wm.render_renamer()
         st.markdown("---")
-        wm.render_lead_sandbox() 
     else:
         st.error("Module functions temporarily offline.")
 
