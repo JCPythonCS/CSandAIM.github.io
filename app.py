efefbd34bb26a025bb0c60e0de21c8afb9a8f70c
JCPython <40036093+JCPythonCS@users.noreply.github.com>
2026-09-25 09:24:07 -0400

    app.py

--- app.py
+++ app.py
@@ -268,7 +268,7 @@ active_panel = st.selectbox(
         "📦 Supply Chain & Logistics (Tab 8)",
         "🏎️ Fleet & Automotive (Tab 9)",
         "🤖 AI-Ops Text Parsing (Tab 10)",
-        "🔒 Cybersecurity & Intrusion (Tab 11)"
+        "🔒 Cybersecurity & Intrusion (Tab 11)",
         "👑 eXtOOLS (Tab 12)"
     ],
     key="cockpit_panel_navigation"
